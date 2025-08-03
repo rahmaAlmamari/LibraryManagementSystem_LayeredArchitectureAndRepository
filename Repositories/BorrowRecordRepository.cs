3edc5c313afd10c96b0696f9a36d3872b338bcc4
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 16:52:54 +0400

    Set BorrowRecordRepository class as public

--- Repositories/BorrowRecordRepository.cs
+++ Repositories/BorrowRecordRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Repositories
 {
-    class BorrowRecordRepository : IBorrowRecordRepository
+    public class BorrowRecordRepository : IBorrowRecordRepository
     {
         //to get all BorrowRecord ..
         public List<BorrowRecord> GetAllBorrowRecords()
