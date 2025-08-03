e0d59cbf8803bcf65595d050974fb6204f5eb14f
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 14:46:53 +0400

    Adding file path for all class

--- FileContext.cs
+++ FileContext.cs
@@ -8,5 +8,9 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository
 {
     public static class FileContext
     {
+        //set the path to the file where the data will be stored ...
+        private static string BookFilePath = "books.json";
+        private static string MemberFilePath = "members.json";
+        private static string BorrowRecordFilePath = "BorrowRecords.json";
     }
 }
