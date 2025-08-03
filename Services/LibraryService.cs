49437db19a8d3efbd41c296df624c3400b098dfc
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 16:46:10 +0400

    Adding AddBook method in LibraryService class

--- Services/LibraryService.cs
+++ Services/LibraryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+using LibraryManagementSystem_LayeredArchitectureAndRepository.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Services
 {
-    public class LibraryService
+    public class LibraryService 
     {
+        private readonly IBookRepository _BookRepository;
+
+        public LibraryService(IBookRepository book_repository)
+        {
+            _BookRepository = book_repository;
+        }
+
+        //to add new book ...
+        public void AddBook(Book book)
+        {
+            _BookRepository.AddBook(book);
+        }
     }
 }
