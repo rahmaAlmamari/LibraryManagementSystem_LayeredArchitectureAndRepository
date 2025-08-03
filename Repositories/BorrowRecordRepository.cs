4efb8bb4ce44da9faadee8a549b54d3083d1bb2f
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 16:22:12 +0400

    Adding GetAllBorrowRecords method to BorrowRecordRepository class

--- Repositories/BorrowRecordRepository.cs
+++ Repositories/BorrowRecordRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Repositories
 {
     class BorrowRecordRepository
     {
+        //to get all BorrowRecord ..
+        public List<BorrowRecord> GetAllBorrowRecords()
+        {
+            return FileContext.LoadBorrowRecords();
+        }
     }
 }
