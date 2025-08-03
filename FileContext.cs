e8bf84199ae31a0e5c656a23beb57f6f1f9099e4
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 14:55:22 +0400

    Add save and load to file for book class

--- FileContext.cs
+++ FileContext.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace LibraryManagementSystem_LayeredArchitectureAndRepository
@@ -12,5 +14,20 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository
         private static string BookFilePath = "books.json";
         private static string MemberFilePath = "members.json";
         private static string BorrowRecordFilePath = "BorrowRecords.json";
+
+        //to load and save book data to file ...
+        public static List<Book> LoadBooks()
+        {
+            if (!File.Exists(BookFilePath))
+                return new List<Book>();
+
+            var json = File.ReadAllText(BookFilePath);
+            return JsonSerializer.Deserialize<List<Book>>(json) ?? new List<Book>();
+        }
+        public static void SaveBooks(List<Book> books)
+        {
+            var json = JsonSerializer.Serialize(books);
+            File.WriteAllText(BookFilePath, json);
+        }
     }
 }
