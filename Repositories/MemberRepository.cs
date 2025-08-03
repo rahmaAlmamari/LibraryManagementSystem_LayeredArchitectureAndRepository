15d745d76f15e7e305846c3ee6f5cae34067c8d1
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 15:40:31 +0400

    Adding GetAllMembers method to MemberRepository class

--- Repositories/MemberRepository.cs
+++ Repositories/MemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Repositories
 {
     class MemberRepository
     {
+        //to get all members ...
+        public List<Member> GetAllMembers()
+        {
+            return FileContext.LoadMembers();
+        }
     }
 }
