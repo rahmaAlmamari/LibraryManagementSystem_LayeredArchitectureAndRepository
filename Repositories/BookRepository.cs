472bcbf3af913ad7f9eb808b883fadf4ad9a08fc
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 15:05:00 +0400

    Adding GetAllBooks method in BookRepository class

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Repositories
 {
     class BookRepository
     {
+        //to get all books ...
+        public List<Book> GetAllBooks()
+        {
+            return FileContext.LoadBooks();
+        }
     }
 }
