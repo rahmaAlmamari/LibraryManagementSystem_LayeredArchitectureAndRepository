8c7e89d1da29c8c0498be92cdf24d4015c3259f4
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 14:55:03 +0400

    Set book class as public class

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Models
 {
-    class Book
+    public class Book
     {
         public int BookId { get; set; }
         public string Title { get; set; }
