9415aa51c670cef564486179b757216f87e713fa
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 14:56:49 +0400

    Set member class as public

--- Models/Member.cs
+++ Models/Member.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Models
 {
-    class Member
+    public class Member
     {
         public int MemberId { get; set; }
         public string Name { get; set; }
