d685c420dc8c92b3b57360979cc21f784f75e147
rahmaAlmamari <alr816125@gmail.com>
2025-08-03 14:40:03 +0400

    Adding book class fields

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,5 +8,9 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Models
 {
     class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
