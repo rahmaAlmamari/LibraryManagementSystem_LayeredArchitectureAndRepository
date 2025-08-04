e252ef2f28bc3f720c8c0fcfaa4ce6af78f7c091
rahmaAlmamari <alr816125@gmail.com>
2025-08-04 09:37:16 +0400

    Adding constructor to BorrowRecord model

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -13,5 +13,11 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Models
         public int BookId { get; set; }
         public DateTime BorrowDate { get; set; }
         public DateTime? ReturnDate { get; set; } // Nullable to allow for books that haven't been returned yet
+        public static int BorrowRecordCount = 0;
+        public BorrowRecord()
+        {
+            BorrowRecordCount++;
+            BorrowRecordId = BorrowRecordCount;
+        }
     }
 }
