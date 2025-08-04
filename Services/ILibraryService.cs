09cb55b299f8fdd53725b6284c381a31e6d23643
rahmaAlmamari <alr816125@gmail.com>
2025-08-04 08:09:38 +0400

    Extract ILibraryService interface again

---
+++ Services/ILibraryService.cs
@@ -0,0 +1,12 @@
+﻿using LibraryManagementSystem_LayeredArchitectureAndRepository.Models;
+
+namespace LibraryManagementSystem_LayeredArchitectureAndRepository.Services
+{
+    public interface ILibraryService
+    {
+        void AddBook(Book book);
+        void BorrowBook(string bookId, string memberId);
+        void RegisterMember(Member member);
+        void ReturnBook(string bookId, string memberId);
+    }
+}
