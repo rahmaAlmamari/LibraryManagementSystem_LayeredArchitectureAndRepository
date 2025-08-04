0947ea3ecc8c279bfae12dc860a6a829a878c848
rahmaAlmamari <alr816125@gmail.com>
2025-08-04 08:20:43 +0400

    Set the base structure for the main menu calling method

--- Program.cs
+++ Program.cs
@@ -7,12 +7,37 @@ namespace LibraryManagementSystem_LayeredArchitectureAndRepository
     {
         static void Main(string[] args)
         {
+            //to create instances of repositories and services
             IBookRepository bookRepository = new BookRepository();
             IMemberRepository memberRepository = new MemberRepository();
             IBorrowRecordRepository borrowRecordRepository = new BorrowRecordRepository();
             ILibraryService libraryService = new LibraryService(bookRepository, memberRepository, borrowRecordRepository);
+            //to create main menu ...
+            char choice;
+            bool exit = true;
+            do
+            {
+                //to display menu options
+                Console.WriteLine("Welcome to the Library Management System");
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Register Member");
+                Console.WriteLine("3. Borrow Book");
+                Console.WriteLine("4. Return Book");
+                Console.WriteLine("5. View All Books");
+                Console.WriteLine("6. View All Members");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Please enter your choice: ");
+                //to get user input
+                choice = char.Parse(Console.ReadLine());
+                //to call the appropriate method based on user choice
+                switch (choice)
+                {
 
 
+
+
+                }
+                } while (exit) ;  
         }
     }
 }
